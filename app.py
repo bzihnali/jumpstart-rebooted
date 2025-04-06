fab53855de2e1389f51d5f91f98d4e4e83badbaa
bytenode <bz5167@rit.edu>
2025-04-05 23:24:49 -0400

    This is a big one! Added HTML+CSS from previous, improved reaction support

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 from slack_sdk import WebClient
 from dotenv import load_dotenv
+import datetime
+from googleapiclient.discovery import build
+import emoji_data_python
 import os
-import re
-import requests
-import json
 
 # Load environment variables
 load_dotenv(dotenv_path=".env", override=True)
@@ -12,15 +12,17 @@ load_dotenv(dotenv_path=".env", override=True)
 app = Flask(__name__)
 client = WebClient(token=os.environ["SLACK_TOKEN"]) # Client for data retrieval from Slack
 
-# Preloads usergroups to prevent overuse of Slack API
+
+# Preloads data to prevent overuse of Slack API
 usergroups = client.usergroups_list(token=os.environ["SLACK_TOKEN"])['usergroups'] 
+emojis = client.emoji_list(token=os.environ["SLACK_TOKEN"], full=True)["emoji"]
 
 """
 Main page, what the client should see
 """
 @app.route("/")
 def index():
-    return "NYI"
+    return render_template("index.html")
 
 """
 Gets the most recent announcement sent and validated in the
@@ -33,28 +35,11 @@ def getLatestAnnouncement():
         conversation_id = None
         subteam_names = {}
 
-        for subteam in client.usergroups_list(token=os.environ["SLACK_TOKEN"])['usergroups']:
-            subteam_names[subteam['id']] = subteam['handle']    
+        for subteam in usergroups:
+            subteam_names[subteam['id']] = subteam['handle']
 
         try:
-            # Set conversation_id to the #announcements channel
-            for result in client.conversations_list():
-                if conversation_id is not None:
-                    break
-                for channel in result["channels"]:
-                    if channel["name"] == channel_name:
-                        conversation_id = channel["id"]
-                        break
-            
-            # Set result to the array of the first limit (currently 1) announcements in #announcements
-            result = client.conversations_history(
-                channel=conversation_id,
-                inclusive=True,
-                oldest="0",
-                limit=1
-            )
-
-            # Get the first message in results (the most recent) and return it as JSON
+            result = getLatestMessagesInChannel("announcements", 1)
             message = result["messages"][0]
             return jsonify(message)
         except Exception as e:
@@ -63,41 +48,29 @@ def getLatestAnnouncement():
     # TODO: Get the most recent message from the proper JSON file (do after the Slack message grabbing is confirmed to work)
     return "NYI"
 
-def getPastAnnouncements(count : int = 1):
-    channel_name = "announcements"
+def getLatestMessagesInChannel(channel_name : str, count : int):
     conversation_id = None
     subteam_names = {}
 
-    for subteam in client.usergroups_list(token=os.environ["SLACK_TOKEN"])['usergroups']:
+    for subteam in usergroups:
         subteam_names[subteam['id']] = subteam['handle']    
 
-    try:
-        # Set conversation_id to the #announcements channel
-        for result in client.conversations_list():
-            if conversation_id is not None:
+    # Set conversation_id to the #announcements channel
+    for result in client.conversations_list():
+        if conversation_id is not None:
+            break
+        for channel in result["channels"]:
+            if channel["name"] == channel_name:
+                conversation_id = channel["id"]
                 break
-            for channel in result["channels"]:
-                if channel["name"] == channel_name:
-                    conversation_id = channel["id"]
-                    break
-        
-        # Set result to the array of the first limit (currently 1) announcements in #announcements
-        result = client.conversations_history(
-            channel=conversation_id,
-            inclusive=True,
-            oldest="0",
-            limit=count
-        )
-
-        # Get the first message in results (the most recent) and return it as a JSON
-        message = result["messages"]
-        return jsonify(message)
-    except Exception as e:
-        print(f"Error: {e}")
     
-    # TODO: Get the most recent message from the proper JSON file (do after the Slack message grabbing is confirmed to work)
-    return "NYI"
-
+    # Set result to the array of the first limit (currently 1) announcements in #announcements
+    return client.conversations_history(
+        channel=conversation_id,
+        inclusive=True,
+        oldest="0",
+        limit=count
+    )
 
 """
 Sends the announcement data to be saved to a JSON file.
@@ -122,7 +95,7 @@ WILL BE REMOVED DOWN THE LINE
 This function currently grabs the elements of the latest announcement.
 """
 @app.route("/parse_latest_announcement")
-def parseLatestAnnouncement(messageToParse : dict = {}):
+def parseLatestAnnouncement():
     # TODO: Parse message info
         # TODO: Check type infos for every block (valid known types: text, rich-text, emoji, usergroup)
         # TODO: Take into account style tag if it exists (valid known tags: bold=true)
@@ -133,7 +106,7 @@ def parseLatestAnnouncement(messageToParse : dict = {}):
 
     return parseAnnouncement(getLatestAnnouncement())
 
-def parseAnnouncement(messageToParse : dict = {}):
+def parseAnnouncement(announcementResponse : dict = {}):
     # TODO: Parse message info
         # TODO: Check type infos for every block (valid known types: text, rich-text, emoji, usergroup)
         # TODO: Take into account style tag if it exists (valid known tags: bold=true)
@@ -142,16 +115,18 @@ def parseAnnouncement(messageToParse : dict = {}):
         # client.usergroups_list(token=os.environ["SLACK_TOKEN"])['usergroups']
         # returns a list of subgroups with keys id (hex value) and handle (@active, @frosh, etc.)
 
-    if messageToParse == {}:
+    if announcementResponse == {}:
         print("No message to parse!")
         return "No message to parse!"
     
     message = ""
 
     # Converts JSON responses to dictionary
-    if type(messageToParse) == Flask.response_class:
-        messageToParse = messageToParse.json
+    if type(announcementResponse) == Flask.response_class:
+        announcementResponse = announcementResponse.json
 
+    messageToParse = announcementResponse
+    
     blocksToParse = []
     if "blocks" in messageToParse:
         blocksToParse = messageToParse["blocks"]
@@ -160,15 +135,12 @@ def parseAnnouncement(messageToParse : dict = {}):
         if "elements" in block:
             message += parseElementList(block['elements'])
 
-    return message
-
-@app.route("/test_parse")
-def testParseFunction():
-    messagesToParse = getPastAnnouncements(25)
-    for message in messagesToParse.json:
-        parseAnnouncement(message)
-
-    return "NYI"
+    return jsonify({
+                    "sender_name": client.users_info(token=os.environ["SLACK_TOKEN"], user=announcementResponse["user"])["user"]["real_name"],
+                    "timestamp": datetime.datetime.fromtimestamp(float(announcementResponse["ts"])).strftime("%m/%d/%Y at %I:%M:%S %p"),
+                    "message_text": message,
+                    "reactions": getReactions(messageToParse["reactions"]) if "reactions" in messageToParse else getReactions([])
+                })
 
 # TODO: Change this to return a built string
 def parseElementList(elementList : list, htmlString : str = ""):
@@ -224,10 +196,46 @@ def buildHTMLfromElement(element : dict):
             print(f"NYI - \'{element['type']}\' - {element}")
     
     return html
+
+def getReactions(reactionArray : list):
+    reactions = ""
+    # client.emoji_list(token=os.environ["SLACK_TOKEN"], full=True)
+    for reaction in reactionArray:
+        if reaction['name'] in emojis and "alias" not in reaction['name']:
+            reactions += f"<span class=\"reaction\"><img src=\"{emojis[reaction['name']]}\">{reaction['count']}</span>"
+        elif reaction['name'].replace("alias:", "") in emojis:
+            reactions += f"<span class=\"reaction\"><img src=\"{reaction['name'].replace('alias:', '')}\">{reaction['count']}</span>"
+        else:
+            reactions += f"<span class=\"reaction\"><span class=\"emoji\">{emoji_data_python.replace_colons(f":{reaction["name"]}:")}</span>{reaction['count']}</span>"
         
+    return reactions
 
 @app.route("/get_events", methods=["GET"])
 def getEvents():
     # TODO: Set up environment variables such that the Google Calendar API can be accessed
     # TODO: Use the Google Calendar API to get a list of events
-    return "NYI"
+    api_key = os.environ["GAPI_KEY"]
+    calendar_id = os.environ["CALENDAR_URL"]
+
+    service = build("calendar", "v3", developerKey=api_key)
+
+    events_result = (
+        service.events()
+        .list(
+            calendarId=calendar_id,
+            timeMin=(datetime.datetime.now(datetime.timezone.utc)).isoformat(),
+            showDeleted=False,
+            singleEvents=True,
+            maxResults=20,
+            orderBy="startTime",
+        )
+        .execute()
+    )
+
+    events = events_result.get("items", [])
+
+    if not events:
+        print("No upcoming events found.")
+        events = "Epic Fail"
+
+    return events
